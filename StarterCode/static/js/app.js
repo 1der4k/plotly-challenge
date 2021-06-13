5ad9c0bca6a4cde6c44d1ee0c1fc65ec2565759b
Zavala <toro.d.c@gmail.com>
2021-06-13 16:44:18 -0400

    h bar graph parameters

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,21 +6,39 @@ function unpack(rows,index) {
     });
 }
 
-d3.json(sample_data)
-        .then(function(data) {
-            console.log(data)
-            var metadata = data.metadata
-            console.log(metadata)
-            var names = data.names
-            console.log(names)
-            var samples = data.samples
-            console.log(samples)
+function buildPlots() {
+    d3.json(sample_data)
+            .then(function(data) {
+                console.log(data)
+                var metadata = data.metadata
+                console.log(metadata)
+                var names = data.names
+                console.log(names)
+                var samples = data.samples
+                console.log(samples)
 
-            var values = samples.map(sample => sample.sample_values)
-            console.log(values)
-            var labels = samples.map(sample => sample.otu_ids)
-            console.log(labels)
-            var hovertext = samples.map(sample => sample.otu_labels)
-            console.log(hovertext)
-})
+                var values = samples.map(sample => sample.sample_values)
+                console.log(values)
+                var labels = samples.map(sample => sample.otu_ids)
+                console.log(labels)
+                var hovertext = samples.map(sample => sample.otu_labels)
+                console.log(hovertext)
 
+                var trace = {
+                    type: "bar",
+                    x: labels,
+                    y: values,
+                    orientation: "h"
+                }
+                
+                var data = [trace]
+                
+                var layout = {
+                    title: "Top 10 Bacteria Cultures Observed"
+                }
+                
+                Plotly.newPlot("bar",data,layout)
+    })
+}
+
+buildPlots()
