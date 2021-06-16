4b178a31ecd55d175fb5f59682fee786784631a2
Zavala <toro.d.c@gmail.com>
2021-06-16 10:49:27 -0400

    modified buildCharts function code to construct metadata panel using new approach

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -22,72 +22,79 @@ function init() {
 
 function buildCharts(data) {
     var dropdown = d3.select("#selDataset");
-    var sample_number = dropdown.property("value")
+    var sample_number = dropdown.property("value");
     // console.log(sample_number)
     
     // console.log(data)
-    var metadata = data.metadata
+    var metadata = data.metadata;
     // console.log(metadata)
-    var samples = data.samples
+    var samples = data.samples;
     // console.log(samples)
     
+    // Build charts
     samples.forEach(function(sample) {
         if (sample.id === sample_number) {
             var otu_ids = sample.otu_ids;
             var sample_values = sample.sample_values;
             var otu_labels = sample.otu_labels;
 
-            console.log(otu_ids)
-            console.log(sample_values)
-            console.log(otu_labels)
-        }
-        
-        // Build horizontal bar graph
-    var trace1 = {
-        type: "bar",
-        x: sample_values.slice(0,10),
-        y: otu_ids.slice(0,10),
-        text: otu_labels,
-        orientation: "h"
-    }
+            console.log(otu_ids);
+            console.log(sample_values);
+            console.log(otu_labels);
 
-    var data1 = [trace1]
+            // Build horizontal bar graph
+            var trace1 = {
+            type: "bar",
+            x: sample_values.slice(0,10),
+            y: otu_ids.slice(0,10),
+            text: otu_labels,
+            orientation: "h"
+            };
 
-    var layout1 = {
-        title: "Top 10 Bacteria Cultures Observed"
-    }
+            var data1 = [trace1];
 
-    Plotly.newPlot("bar",data1,layout1)
+            var layout1 = {
+                title: "Top 10 Bacteria Cultures Observed"
+            };
 
-    // Build bubble chart
-    var trace2 = {
-        x: otu_ids,
-        y: sample_values,
-        mode: "markers",
-        marker: {
-            size: sample_values
-        }
-    }
+            Plotly.newPlot("bar",data1,layout1);
 
-    var data2 = [trace2]
+            // Build bubble chart
+            var trace2 = {
+                x: otu_ids,
+                y: sample_values,
+                mode: "markers",
+                marker: {
+                    size: sample_values
+                }
+            };
 
-    var layout2 = {
-        title: "Bacteria Cultures Per Sample"
-    }
+            var data2 = [trace2];
 
-    Plotly.newPlot("bubble",data2,layout2)
+            var layout2 = {
+                title: "Bacteria Cultures Per Sample"
+            };
+
+            Plotly.newPlot("bubble",data2,layout2);
+
+            
+        } 
+    });
 
     // Build metadata chart
-    var panel = d3.select("#sample-metadata")
+    var panel = d3.select("#sample-metadata");
 
-    var default_sample = metadata[0]
-
-    // console.log(default_sample)
-    
-    for ([key,value] of Object.entries(default_sample)) {
-        panel.append("p").text(`${key} : ${value}`)
-    }
+    metadata.forEach(function(sample) {
+        if (sample.id === +sample_number) {
+            console.log(sample.id);
+            
+            for ([key,value] of Object.entries(sample)) {
+            panel.append("p").text(`${key} : ${value}`);
+            }
+        }
     })
+};    
+    
 
     // var values = samples.map(sample => sample.sample_values)
     // // console.log(values)
@@ -103,7 +110,7 @@ function buildCharts(data) {
     // var hovertext = hovertexts[0]
     
     
-}
+
            
 
 // function eventHandler() {
